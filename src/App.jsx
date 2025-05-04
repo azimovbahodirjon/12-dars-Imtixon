cdb8039554c52d66a7caef20a8e9a80714daa238
Bahodirjon <Bahodirjon@gmail.com>
2025-05-04 17:39:29 +0300

    Add initial project setup with Tailwind CSS, Redux store, and various SVG assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import Center from "./components/Center";
+import Header from "./components/Header";
+import { AnimatePresence } from "framer-motion";
+import { Routes, Route, useLocation } from "react-router-dom";
+import InvoiceInfo from "./components/InvoiceInfo";
+import invoiceSlice from "./app/invoiceSlice";
+import { useDispatch } from "react-redux";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  const onDelete = (id) => {
+    dispatch(invoiceSlice.actions.deleteInvoice({ id: id }));
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className=" dark:bg-[#141625] duration-300 min-h-screen bg-[#f8f8fb]">
+      <Header />
+
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route element={<Center />} path="" />
+          <Route
+            element={<InvoiceInfo onDelete={onDelete} />}
+            path="/invoice"
+          />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
 }
 
-export default App
+export default App;
